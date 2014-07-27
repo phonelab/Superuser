9cc6dda4f2061b69e8961fe48e65f0e0c8fdc748
Jinghao Shi <jinghaos@buffalo.edu>
2014-07-27 00:23:38 -0400

    do not check su binary

--- Superuser/src/com/koushikdutta/superuser/util/SuHelper.java
+++ Superuser/src/com/koushikdutta/superuser/util/SuHelper.java
@@ -6,6 +6,14 @@ import android.util.Log;
 public class SuHelper {
     public static String CURRENT_VERSION = "16";
     public static void checkSu(Context context) throws Exception {
+        /**
+         * This code snipet does not work: even with su installed, "su -v" does
+         * not return version code.
+         *
+         * Plus, we don't really care if su binary is updated or not. We'll not
+         * update Superuser.apk or su binary anyway.
+         */
+        /*
         Process p = Runtime.getRuntime().exec("su -v");
         String result = Settings.readToEnd(p.getInputStream());
         Log.i("Superuser", "Result: " + result);
@@ -19,5 +27,6 @@ public class SuHelper {
         String[] parts = result.split(" ");
         if (!CURRENT_VERSION.equals(parts[0]))
             throw new Exception("binary is old");
+            */
     }
 }
